71e887c01fe6b66372b805ac8c95789706564375
Fede <federicogarciaherwig@gmail.com>
2026-04-07 23:05:11 +0200

    chore: update demo admin seed

--- database/seed.sql
+++ database/seed.sql
@@ -2,7 +2,7 @@ INSERT INTO utilisateurs (nom, email, mot_de_passe, role, prenom)
 VALUES (
   'Admin',
   'admin@fournier-roux.local',
-  '$2y$10$urMajGO/XKVhAUSggWOsJuxbIK5dF.ayUo2EEy0vQLHz3JmuBbMTC',
+  '$2y$10$ZIK.t2Otc3TajGzeiliL5e14BOdQminnJuTogypRNei1iecKh20C6',
   'admin',
   'Test'
 );
