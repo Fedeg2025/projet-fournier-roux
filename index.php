d7522e367ba23e95d22550d2b078be257b61e978
Fede <federicogarciaherwig@gmail.com>
2026-04-21 10:52:42 +0200

    Mise à jour des fichiers : modifications du site, styles et images

--- index.php
+++ index.php
@@ -11,7 +11,10 @@
 // =========================
 // DÉMARRAGE DE LA SESSION
 // =========================
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_name('fournier_roux');
+    session_start();
+}
 
 
 // =========================
